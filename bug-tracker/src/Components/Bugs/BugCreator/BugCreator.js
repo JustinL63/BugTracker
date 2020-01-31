4fe496d77966e0982e96f05ceee7d901c73e9678
Justin Lamb <lambjg63@gmail.com>
2020-01-31 10:55:30 -0500

    hookform

--- bug-tracker/src/Components/Bugs/BugCreator/BugCreator.js
+++ bug-tracker/src/Components/Bugs/BugCreator/BugCreator.js
@@ -1,65 +1,32 @@
-import React, { Component } from "react"
+import React from "react"
 import classes from "./BugCreator.module.css"
+import { useForm } from "react-hook-form"
 
-class BugCreator extends Component {
-    state = {
-        bug: {
-            title: "",
-            description: "",
-            assignedTo: {
-                elementType: "select",
-                elementConfig: {
-                   options: [
-                       {value: "justin", displayValue: "Justin"},
-                       {value: "john", displayValue: "John"}
-                   ]
-                },
+const BugCreator = (props) => {
 
-            },
-           
-        }
-    }
+    const { register, handleSubmit } = useForm()
+    const onSubmit = data => console.log(data)
 
-    onSubmitHandler = (event)=> {
-       alert("A name was submitted " + this.state.value)
-       event.preventDefault()
-    }
-
-    onChangeHandler = (event) => {
-        this.setState({value: event.target.value})
-
-    }
-
-    render() {
-        return (
-            <div className={classes.BugCreator}>
-                <form>
-                    <label>Title of Bug</label>
-                        <input 
-                            type="text" 
-                            name="title" placeholder="title"
-                            value={this.state.value} 
-                            onChange={this.onChangeHandler}></input>
-                        <br/>
-                    <label>Description of Bug</label>
-                        <textarea type="text" 
-                            name="description" 
-                            placeholder="description"
-                            value={this.state.value} 
-                            onChange={this.onChangeHandler}></textarea>
-                        <br/>
-                    <label>Assigned To</label>
-                        <select>
-                            <option value="justin">Justin</option>
-                            <option value="john">John</option>
-                            <option value="consuela">Consuela</option>
-                        </select>
-                        <br/>
-                    <button onClick={this.onSubmitHandler}>Submit</button>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div className={classes.BugCreator}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Title</label>
+                    <input type="text" placeholder="Title" name="title" ref={register} />
+                    <br/>
+                <label>Description</label>
+                    <textarea type="text" placeholder="Title" name="description" ref={register} />
+                <label>Assigned To:</label>
+                    <select name="assignedTo" ref={register}>
+                        <option value="Tom">Tom</option>
+                        <option value="Dick">Dick</option>
+                        <option value="Harry">Harry</option>
+                    </select>
+                    <br/>
+                <input type="submit" />
+            </form>
+        </div>
+    )
 }
+
 
 export default BugCreator
